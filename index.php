38f074ba1fdd1017b5cdd9ffc4ad5da8c5e0defb
sofiafuertes <sofia_fuertes@hotmail.com>
2024-12-19 18:54:07 +0100

    project structure

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+//* J'active la session
+session_start();
+//* Inlclure ressources communes a chaque route
+
+//Analyse de l'URL avec parse_url() et retourne ses composants
+$url = parse_url($_SERVER['REQUEST_URI']);
+
+//test soit l'url a une route sinon on renvoi à la racine
+$path = isset($url['path']) ? $url['path'] : '/';
+
+/*--------------------------ROUTER -----------------------------*/
+
+//test de la valeur $path dans l'URL et import de la ressource
+switch ($path) {
+
+    case $path === "/ByKate_Stage/":
+        include './view/header_view.php';
+        include './view/inicio_view.php';
+        include './view/footer_view.php';
+        break;  
+    }
