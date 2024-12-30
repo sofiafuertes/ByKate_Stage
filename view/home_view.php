71eeccc336b261fa71ba32a4709acb1ad4f7abbe
sofiafuertes <sofia_fuertes@hotmail.com>
2024-12-30 12:37:49 +0100

    section AboutUs homepage

--- view/home_view.php
+++ view/home_view.php
@@ -3,7 +3,7 @@
     </div>
 
     <div class="productsSection">
-        <h1>NUESTROS PRODUCTOS</h1>
+        <h1>Nuestros Productos</h1>
         <div class="productsContainer">
             <div class="imgProduct">
                 <img src="./photos/products/chocotorta.jpg" alt="Foto chocotorta">
@@ -18,8 +18,43 @@
                 <img src="./photos/products/pavlovaddl.jpg" alt="Foto Pavlovla de dulce de leche">
             </div>
         </div>
-        <div class="buttonProduct">
+        <div class="button">
             <button><a href="/ByKate_Stage/nuestromenu">Todos los productos</a></button>
         </div>
     </div>
+
+    <div class="aboutUsSection">
+        <div class="aboutUsContainer">
+            <div class="titleTextFlex">
+                <h1>Nuestra Historia</h1>
+                <div class="aboutUsText">
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis, est. Lorem ipsum dolor sit
+                        amet
+                        consectetur adipisicing elit. Amet nulla molestiae consequatur nemo earum eligendi, odit iusto
+                        tempore voluptatibus ratione placeat sed, est vitae at, quasi corrupti dolor eum porro autem
+                        laborum
+                        ut? Temporibus quae, eos veniam illo expedita illum. Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quam, maxime.Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                        Corporis, est. Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Amet nulla molestiae consequatur nemo earum eligendi, odit iusto
+                        tempore voluptatibus ratione placeat sed, est vitae at, quasi corrupti dolor eum porro autem
+                        laborum
+                        ut? Temporibus quae, eos veniam illo expedita illum. Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quam, maxime.Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                        Corporis, est. Lorem ipsum dolor sit amet
+                        consectetur adipisicing elit. Amet nulla molestiae consequatur nemo earum eligendi, odit iusto
+                        tempore voluptatibus ratione placeat sed, est vitae at, quasi corrupti dolor eum porro autem
+                        laborum
+                        ut? Temporibus quae, eos veniam illo expedita illum. Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quam, maxime.</p>
+                </div>
+                <div class="button">
+            <button><a href="/ByKate_Stage/quienessomos">Continuar leyendo</a></button>
+        </div>
+            </div>
+            <div class="aboutUsImg">
+                <img src="./photos/aboutUsHome.jpg" alt="Foto de la pastelería">
+            </div>
+            
+        </div>
 </main>
